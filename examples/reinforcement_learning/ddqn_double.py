410a7ef78dffa823faecef77a8fca7ea0dfcb6cf
Davide Medaglia <davidemedaglia29@hotmail.it>
2022-01-17 17:55:39 +0100

    fix linear bin and unbin for steering and throttle, change last dense layer

--- examples/reinforcement_learning/ddqn_double.py
+++ examples/reinforcement_learning/ddqn_double.py
@@ -91,8 +91,8 @@ class DDQNAgent:
         x = Dense(50, activation='relu', name='dense_3')(x)
         x = Dropout(rate=0.2)(x)
 
-        steering_out = Dense(15, activation='linear', name='steering_out')(x)
-        throttle_out = Dense(9, activation='sigmoid', name='throttle_out')(x)
+        steering_out = Dense(20, activation='linear', name='steering_out')(x)
+        throttle_out = Dense(10, activation='linear', name='throttle_out')(x)
         model = Model(inputs=[img_in], outputs=[steering_out, throttle_out])
         adam = Adam(lr=self.learning_rate)
         model.compile(loss="mse", optimizer=adam)
@@ -127,12 +127,12 @@ class DDQNAgent:
         else:
             # print("Return Max Q Prediction")
             q_value = self.model.predict(s_t)
-            # print("get_action() prediction: Steering: ", linear_unbin(q_value[0][0], 15), 
-            #     "\tTrottle: ", linear_unbin(q_value[1][0], 9))
+            # print("get_action() prediction: Steering: ", linear_unbin_steering(q_value[0][0]),
+            #     "\tTrottle: ",linear_unbin_throttle(q_value[1][0]), " bins:", q_value[1][0])
             
             # Convert q array to steering value and throttle value
-            actions.append(linear_unbin(q_value[0][0], 15))
-            actions.append(linear_unbin(q_value[1][0], 9))
+            actions.append(linear_unbin_steering(q_value[0][0]))
+            actions.append(linear_unbin_throttle(q_value[1][0]))
 
         return actions
 
@@ -181,40 +181,30 @@ class DDQNAgent:
         self.model.save_weights(name)
 
 # Utils Functions #
-
-def linear_bin(a, n_bin):
-    """
-    Convert a value to a categorical array.
-
-    Parameters
-    ----------
-    a : int or float
-
-    Returns
-    -------
-    list of int
-        A list of length 15 with one item set to 1, which represents the linear value, and all other items set to 0.
-    """
-    a = a + 1
-    b = round(a / (2 / (n_bin-1)))
-    arr = np.zeros(n_bin)
+def linear_bin_steering(val):
+    bins = np.linspace(-1, 1, 20)
+    b = np.digitize(val, bins, True)
+    arr = np.zeros(20)
     arr[int(b)] = 1
     return arr
 
-def linear_unbin(arr, n_bin):
-    """
-    Convert a categorical array to value.
-
-    See Also
-    --------
-    linear_bin
-    """
-    if not len(arr) == n_bin:
-        raise ValueError("Illegal array length, must be 15")
+def linear_unbin_steering(arr):
     b = np.argmax(arr)
-    a = b * (2 / (n_bin-1)) - 1
-    return a
+    bins = np.linspace(-1, 1, 20)
+    return bins[int(b)]
 
+def linear_bin_throttle(val):
+    bins = np.linspace(0, 1, 10)
+    b = np.digitize(val, bins, True)
+    arr = np.zeros(10)
+    arr[int(b)] = 1
+    return arr
+
+def linear_unbin_throttle(arr):
+    b = np.argmax(arr)
+    bins = np.linspace(0, 1, 10)
+    return bins[int(b)]
+    
 
 def run_ddqn(args):
     """
@@ -276,14 +266,14 @@ def run_ddqn(args):
         agent.model.summary()
 
         name_model = args.model.replace(".h5", "")
-        file = open(str(name_model)+"_metric.csv", "w+")
+        file = open(str(name_model) + "_metric.csv", "w+")
         log = writer(file)
-        log.writerow(['Episode','Timestep', 'Avg Steer', 'Min Reward', 
-        'Avg Reward', 'Max Reward','Reward Sum', 'Episode Length',  
-        'Max Q steering', 'Max Q throttle', 'Epsilon','Episode Time', 
-        'Avg Speed','Max Speed','Min CTE','Avg CTE','Max CTE','Distance', 
-        "Average Throttle", "Max Throttle", "Min Throttle",
-        "Average Absolute CTE", "Min Absolute CTE", "Max Absolute CTE"])
+        log.writerow(['Episode', 'Timestep', 'Avg Steer', 'Min Reward',
+                      'Avg Reward', 'Max Reward', 'Reward Sum', 'Episode Length',
+                      'Max Q steering', 'Max Q throttle', 'Epsilon', 'Episode Time',
+                      'Avg Speed', 'Max Speed', 'Min CTE', 'Avg CTE', 'Max CTE', 'Distance',
+                      "Average Throttle", "Max Throttle", "Min Throttle",
+                      "Average Absolute CTE", "Min Absolute CTE", "Max Absolute CTE"])
 
         for e in range(EPISODES):
             print("START EPISODE: ", e)
@@ -327,28 +317,27 @@ def run_ddqn(args):
                 # Get action for the current state and go one step in environment
                 action = agent.get_action(s_t)
                 next_obs, reward, done, info = env.step(action)
-
                 x_t1 = agent.process_image(next_obs)
                 x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1)  
                 s_t1 = np.append(x_t1, s_t[:, :, :, :img_frames-1], axis=3)  
 
                 # Save the sample <s, a, r, s'> to the replay memory
-                steering_bin = np.argmax(linear_bin(action[0], 15))
-                throttle_bin = np.argmax(linear_bin(action[1], 9))
+                steering_bin = np.argmax(linear_bin_steering(action[0]))
+                throttle_bin = np.argmax(linear_bin_throttle(action[1]))
                 agent.replay_memory(s_t, [steering_bin, throttle_bin],  reward, s_t1, done)
                 agent.update_epsilon()
 
                 if agent.train:
                     agent.train_replay()
-                    # stats
-                    steers.append(action[0])
-                    throttles.append(action[1])
-                    velocities.append(round(info["speed"], 4))
-                    rewards.append(round(reward,4))
-                    ctes.append(round(info["cte"], 4))
-                    ctes_absolute.append(round(abs(info["cte"]), 4))
-                    distance += info["speed"]*(time.time()-distance_time)
-                    distance_time = time.time()
+                # stats
+                steers.append(action[0])
+                throttles.append(action[1])
+                velocities.append(round(info["speed"], 4))
+                rewards.append(round(reward,4))
+                ctes.append(round(info["cte"], 4))
+                ctes_absolute.append(round(abs(info["cte"]), 4))
+                distance += info["speed"]*(time.time()-distance_time)
+                distance_time = time.time()
 
                 s_t = s_t1
                 agent.t = agent.t + 1
@@ -364,7 +353,6 @@ def run_ddqn(args):
                     # Save model for each episode
                     if agent.train:
                         agent.save_model(args.model)
-
                         # stat save
                         log.writerow([e,agent.t, round(np.mean(steers), 4), round(np.min(rewards), 4), 
                             round(np.mean(rewards), 4), round(np.max(rewards), 4), round(np.sum(rewards),4), 
